cf76be547b6df245a1c26bdfbe46a778550173d6
OwaisPaul <ovaish25@gmail.com>
2025-05-28 21:46:09 +0530

    setup test env

--- utils/tests/blog_api.test.js
+++ utils/tests/blog_api.test.js
@@ -250,3 +250,25 @@ describe('most liked', () => {
   assert.deepStrictEqual(result, expected)
     })
 })
+
+describe('tests for the blog app', () => {
+  const { test, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
+
+})
+
