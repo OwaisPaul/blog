2d755f828dde206ce67f9afe30ee30362797acd2
OwaisPaul <ovaish25@gmail.com>
2025-06-16 10:35:02 +0530

    Blog list expansion step 6

---
+++ controllers/login.js
@@ -0,0 +1,37 @@
+
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  try {
+    const { username, password } = request.body
+    const user = await User.findOne({ username })
+
+    // Check if user exists and password is correct
+    const passwordCorrect = user
+      ? await bcrypt.compare(password, user.passwordHash)
+      : false
+
+    if (!user || !passwordCorrect) {
+      return response.status(401).json({ error: 'invalid username or password' })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    response.status(200).send({ token, username: user.username, name: user.name })
+
+  } catch (error) {
+    console.error('Login error:', error)
+    response.status(500).json({ error: 'internal server error' })
+  }
+})
+
+
+module.exports = loginRouter
